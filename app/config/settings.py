f3dd7f7c5c0af21103662c4ffb1f1ff65387336e
Eduardo Czamanski Rota <eduardoczamanski@protonmail.com>
2023-06-18 18:55:10 -0300

    feat: adicionado url da api da fipe e ajuste nas variaveis de ambiente

--- app/config/settings.py
+++ app/config/settings.py
@@ -9,14 +9,23 @@ filepath = pathlib.Path(__file__).resolve().parent.parent
 
 load_dotenv(str(filepath) + "/.env")
 
-
 class Settings(BaseSettings):
     debug: bool = os.getenv("DEBUG")
     app_name: str = "Google Cloud API Example"
     version: str = "0.0.1"
     prefix_v1: str = "/api/v1"
     
-    google_certificate_path: str = os.getenv("GOOGLE_CERTIFICATE_PATH")
+    gcp_certificate_path: str = os.getenv("GCP_CERTIFICATE_PATH")
+    project: str = os.getenv("GCP_PROJECT_ID")
+    region: str = os.getenv("GCP_REGION")
+    queue_name: str = f"{project}-queue"
+    queue: str = f"projects/{project}/locations/{region}/queues/{queue_name}"
+    service_name: str = os.getenv("GCP_RUN_SERVICE_NAME")
+    service: str = f"projects/{project}/locations/{region}/services/{service_name}"
+    
+    # Integração API FIPE https://deividfortuna.github.io/fipe/
+    fipe_url: str = os.getenv("FIPE_BASE_URL")
+    
 
 settings = Settings()
 
