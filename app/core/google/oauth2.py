64c0c707c4dd2721c50e49c7d557b7662f04cfc3
Eduardo Czamanski Rota <eduardoczamanski@protonmail.com>
2023-06-18 19:09:43 -0300

    feat: ajuste credentials apenas para desenvolvimento

--- app/core/google/oauth2.py
+++ app/core/google/oauth2.py
@@ -2,4 +2,7 @@ from google.oauth2 import service_account
 
 from app.config.settings import settings
 
-credentials = service_account.Credentials.from_service_account_file(settings.gcp_certificate_path)
+credentials = None
+
+if settings.gcp_certificate_path:
+  credentials = service_account.Credentials.from_service_account_file(settings.gcp_certificate_path)
