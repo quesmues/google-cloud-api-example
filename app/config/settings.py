c44dff67c652cf088808eb20c1208835b3d2533e
Eduardo Czamanski Rota <eduardoczamanski@protonmail.com>
2023-06-18 20:10:22 -0300

    feat: adicionado secret_key, e configurado variaveis de ambiente no build

--- app/config/settings.py
+++ app/config/settings.py
@@ -15,13 +15,20 @@ class Settings(BaseSettings):
     version: str = "0.0.2"
     prefix_v1: str = "/api/v1"
     
+    # Certificado apenas em ambiente local
     gcp_certificate_path: str = os.getenv("GCP_CERTIFICATE_PATH")
+    
+    # Auth Scopes
+    scopes: list = [os.getenv("GCP_AUTH_SCOPE")]
+    
     project: str = os.getenv("GCP_PROJECT_ID")
     region: str = os.getenv("GCP_REGION")
     queue_name: str = f"{project}-queue"
     queue: str = f"projects/{project}/locations/{region}/queues/{queue_name}"
     service_name: str = os.getenv("GCP_RUN_SERVICE_NAME")
     service: str = f"projects/{project}/locations/{region}/services/{service_name}"
+    
+
     
     # Integração API FIPE https://deividfortuna.github.io/fipe/
     fipe_url: str = os.getenv("FIPE_BASE_URL")
--- app/core/google/oauth2.py
+++ app/core/google/oauth2.py
@@ -4,5 +4,6 @@ from app.config.settings import settings
 
 try:
   credentials = service_account.Credentials.from_service_account_file(settings.gcp_certificate_path)
-except TypeError:
-  credentials = None
+except Exception:
+  import google.auth
+  credentials, project_id = google.auth.default(scopes=settings.scopes)
